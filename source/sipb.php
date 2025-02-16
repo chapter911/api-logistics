78d1079789c0ec01c8f0ebfb522fd1e501a4234a
chapter911 <51829448+chapter911@users.noreply.github.com>
2025-02-16 09:45:59 +0700

    Change search method to GET and add getSPJ method for fetching SPJ data

--- source/sipb.php
+++ source/sipb.php
@@ -12,7 +12,7 @@ class sipb extends Library {
     }
 
     private function search() {
-        $this->app->post($this->pattern, function (Request $request, Response $response) {
+        $this->app->get($this->pattern, function (Request $request, Response $response) {
             $value_data = $request->getParsedBody();
             $no_spj = $value_data['no_spj'];
 
@@ -30,6 +30,28 @@ class sipb extends Library {
                 $result['data']    = null;
             }
             return $response->withJson($result, 200);
-        })->add(parent::middleware());
+        });
+    }
+
+    private function getSPJ() {
+        $this->app->get($this->pattern . "/{no_spj}", function (Request $request, Response $response, $args) {
+            $value_data = $request->getParsedBody();
+            $no_spj = $args['no_spj'];
+
+            $sql = "SELECT * FROM vw_sipb_hdr WHERE no_spj = '$no_spj'";
+            $fetch = $this->db->query($sql)->fetchAll();
+            if ($fetch) {
+                $material = $this->db->query("SELECT * FROM vw_sipb_dtl WHERE no_spj = '$no_spj'")->fetchAll();
+                $result['status']  = "success";
+                $result['message'] = "berhasil mendapatkan data";
+                $result['data']['header']    = $fetch;
+                $result['data']['material']    = $material;
+            } else {
+                $result['status']  = "failed";
+                $result['message'] = "gagal mendapatkan data";
+                $result['data']    = null;
+            }
+            return $response->withJson($result, 200);
+        });
     }
 }
